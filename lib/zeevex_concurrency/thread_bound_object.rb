a3ab2b9415422bdae34c256a0785521b353bc458
Robert Sanders <robert@zeevex.com>
2013-01-07 16:16:43 -0500

    wrapper for objects to prevent access from multiple threads

---
+++ lib/zeevex_concurrency/thread_bound_object.rb
@@ -0,0 +1,125 @@
+require 'zeevex_concurrency'
+module ZeevexConcurrency
+  class BindingError < StandardError; end
+
+  #
+  # This is a wrapper for objects which allows them to be in one of 3 states:
+  #
+  #  Bound to a thread
+  #  Unbound
+  #  Nullbound
+  #
+  # When `unbound`, the object will allow messages from any thread, but will transition
+  # to bound(thread_x) after receiving a message from thread_x
+  #
+  # When in state `bound(thread_x)`, it will *only* accept messages from thread_x. Messages
+  # from other threads will cause a BindingError. This includes must binding management
+  # messages - in other words, a thread can unbind its own objects; but they cannot be stolen.
+  #
+  # When in state `nullbound`, it will not allow messages from any thread, other than those
+  # associated with binding management.
+  #
+  class ThreadBoundObject < SimpleDelegator
+    #
+    # Eagerly bind this object to a thread
+    #
+    def self.bind(tbo, thread)
+      tbo.__bind_to_thread(thread)
+    end
+
+    #
+    # Free the object from a binding; will auto-bind to first thread to send
+    # it a message
+    #
+    def self.unbind(tbo)
+      tbo.__unbind
+    end
+
+    #
+    # Prevent the object from receiving messages from ANY thread; used for
+    # objects "in transit" between threads, e.g. on a channel or queue
+    #
+    def self.nullbind(tbo)
+      tbo.__nullbind
+    end
+
+    def self.bound?(tbo)
+      tbo.__bound?
+    end
+
+    #
+    # Prevent the object from receiving messages from ANY thread; used for
+    # objects "in transit" between threads, e.g. on a channel or queue
+    #
+
+    def initialize(obj, thread = nil)
+      super(obj)
+      __bind_to_thread(thread) if thread
+    end
+
+    def __getobj__(ignore_binding = false)
+      __check_binding unless ignore_binding
+      super()
+    end
+
+    def __setobj__(obj)
+      raise "__setobj__ not supported"
+    end
+
+    def __bind_to_thread(thr)
+      raise ArgumentError, "Must provide thread" unless thr && thr.is_a?(Thread)
+      raise BindingError,  "Object is already bound" if __bound?
+      __check_bindability
+      @bound_thread_id = thr.__id__
+    end
+
+    def __unbind
+      __check_bindability
+      @bound_thread_id = nil
+    end
+
+    #
+    # Bind this to a non-existent value so that it cannot be invoked from anywhere;
+    # unbind and bind can override this
+    #
+    def __nullbind
+      __check_bindability
+      # this can never match Thread.current
+      @bound_thread_id = :none
+    end
+
+    def __bound?
+      @bound_thread_id && @bound_thread_id != :none
+    end
+
+    def __bindable?
+      @bound_thread_id.nil? || @bound_thread_id == :none
+    end
+
+    private
+
+
+    #
+    # Successful if thread unbound or this method is being called from bound thread
+    #    if unbound, auto-bind this object to the calling thread
+    # raise BindingError otherwise
+    #
+    def __check_binding
+      case @bound_thread_id
+      when Thread.current.__id__
+        true
+      when nil
+        @bound_thread_id = Thread.current.__id__
+      else
+        raise BindingError, "Object is bound to #{@bound_thread_id.inspect}"
+      end
+    end
+
+    def __check_bindability
+      return true if @bound_thread_id == :none || @bound_thread_id.nil?
+      return true if @bound_thread_id == Thread.current.__id__
+      raise BindingError,  "Object is bound to another thread and cannot be re- or un-bound"
+    end
+
+  end
+end
---
+++ spec/thread_bound_object.rb
@@ -0,0 +1,190 @@
+require File.join(File.dirname(__FILE__), 'spec_helper')
+require 'zeevex_concurrency/thread_bound_object.rb'
+require 'thread'
+
+describe ZeevexConcurrency::ThreadBoundObject do
+  clazz = ZeevexConcurrency::ThreadBoundObject
+
+  let :obj do
+    mock()
+  end
+
+  let :thread do
+    Thread.new do
+      sleep
+    end
+  end
+
+  before do
+    obj
+    thread
+  end
+
+  after do
+    thread.kill
+  end
+
+  # around :each do |ex|
+  #   Timeout::timeout(30) do
+  #     ex.run
+  #   end
+  # end
+
+  context 'argument checking' do
+    it 'should require an object' do
+      expect { clazz.new }.
+        to raise_error(ArgumentError)
+    end
+
+    it 'should accept a thread to pre-bind to' do
+      expect {
+        clazz.new(obj, thread)
+      }.not_to raise_error
+    end
+
+    it 'should not accept a non-thread object to pre-bind to' do
+      expect { clazz.new(obj, "foo") }.
+        to raise_error(ArgumentError)
+    end
+  end
+
+  context 'when explicitly bound to this thread' do
+    subject do
+      clazz.new(obj, Thread.current)
+    end
+    it 'should be bound' do
+      subject.__bound?.should be_true
+    end
+    it 'should allow messages' do
+      obj.should_receive(:foo)
+      subject.foo
+    end
+    it 'should allow unbinding' do
+      subject.__unbind
+      subject.__bound?.should be_false
+    end
+    it 'should allow unbinding' do
+      subject.__nullbind
+      subject.__bound?.should be_false
+    end
+  end
+
+  context 'when explicitly bound to other thread' do
+    subject do
+      clazz.new(obj, thread)
+    end
+    it 'should be bound' do
+      subject.__bound?.should be_true
+    end
+    it 'should not allow messages' do
+      expect { subject.foo }.
+        to raise_error(ZeevexConcurrency::BindingError)
+    end
+    it 'should not allow unbinding' do
+      expect { subject.__unbind }.
+        to raise_error(ZeevexConcurrency::BindingError)
+    end
+    it 'should not allow nullbinding' do
+      expect { subject.__nullbind }.
+        to raise_error(ZeevexConcurrency::BindingError)
+    end
+    it 'should not allow binding to another thread' do
+      expect { subject.__bind_to_thread(Thread.current) }.
+        to raise_error(ZeevexConcurrency::BindingError)
+    end
+  end
+
+  context 'when unbound' do
+    subject do
+      clazz.new(obj)
+    end
+    it 'should not be bound' do
+      subject.__bound?.should be_false
+    end
+    it 'should allow messages' do
+      obj.should_receive(:foo)
+      subject.foo
+    end
+    it 'should auto-bind on first message receipt' do
+      obj.should_receive(:foo)
+      subject.foo
+      subject.__bound?.should be_true
+    end
+    it 'should not allow messages from another thread after first message receipt' do
+      obj.should_receive(:foo)
+      Thread.new { subject.foo }.join
+      expect { subject.foo }.to raise_error(ZeevexConcurrency::BindingError)
+    end
+    it 'should allow unbinding' do
+      expect { subject.__unbind }.
+        not_to raise_error
+    end
+    it 'should allow nullbinding' do
+      expect { subject.__nullbind }.
+        not_to raise_error
+      subject.__bound?.should be_false
+    end
+    it 'should allow binding to another thread' do
+      expect { subject.__bind_to_thread(Thread.current) }.
+        not_to raise_error
+      subject.__bound?.should be_true
+    end
+  end
+
+  context 'when nullbound' do
+    subject do
+      clazz.new(obj)
+    end
+    before do
+      subject.__nullbind
+    end
+    it 'should not be bound' do
+      subject.__bound?.should be_false
+    end
+    it 'should not allow messages' do
+      expect { subject.foo }.
+        to raise_error(ZeevexConcurrency::BindingError)
+    end
+    it 'should remain nullbound after message' do
+      expect { subject.foo }.
+        to raise_error(ZeevexConcurrency::BindingError)
+      subject.__bound?.should be_false
+    end
+    it 'should allow unbinding' do
+      expect { subject.__unbind }.
+        not_to raise_error
+    end
+    it 'should allow nullbinding' do
+      expect { subject.__nullbind }.
+        not_to raise_error
+    end
+    it 'should allow binding to another thread' do
+      expect { subject.__bind_to_thread(Thread.current) }.
+        not_to raise_error
+    end
+  end
+
+  context 'class methods' do
+    it '.bind should call __bind_to_thread__ on object' do
+      obj.should_receive(:__bind_to_thread).with(thread)
+      clazz.bind(obj, thread)
+    end
+
+    it '.unbind should call __unbind on object' do
+      obj.should_receive(:__unbind)
+      clazz.unbind(obj)
+    end
+
+    it '.nullbind should call __nullbind on object' do
+      obj.should_receive(:__nullbind)
+      clazz.nullbind(obj)
+    end
+
+    it '.bound? should call __bound? on object' do
+      obj.should_receive(:__bound?)
+      clazz.bound?(obj)
+    end
+  end
+
+end
+
