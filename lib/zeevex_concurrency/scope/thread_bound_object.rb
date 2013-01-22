d841374fb7ae9fb894831b077e642fdd4aabc585
Robert Sanders <robert@zeevex.com>
2013-01-21 22:17:36 -0500

    docs for TBO

--- lib/zeevex_concurrency/scope/thread_bound_object.rb
+++ lib/zeevex_concurrency/scope/thread_bound_object.rb
@@ -22,8 +22,13 @@ module ZeevexConcurrency
   class ThreadBoundObject < SimpleDelegator
     #
     # Eagerly bind this object to a thread. If nil or no thread arg provided,
-    # binds to current thread
+    # binds to current thread.
     #
+    # @param [ThreadBoundObject] tbo the ThreadBoundObject to act upon
+    # @param [Thread] thread the Thread to bind to, or nil for Thread.current
+    # @return [ThreadBoundObject] the ThreadBoundObject
+    #
+    # @raise BindingError if already bound by another thread
     def self.bind(tbo, thread = nil)
       tbo.__bind_to_thread(thread)
     end
@@ -32,6 +37,9 @@ module ZeevexConcurrency
     # Free the object from a binding; will auto-bind to first thread to send
     # it a message
     #
+    # @param [ThreadBoundObject] tbo the TBO to unbind
+    # @return [ThreadBoundObject] the same object
+    # @raise BindingError if bound by another thread
     def self.unbind(tbo)
       tbo.__unbind
     end
@@ -40,10 +48,17 @@ module ZeevexConcurrency
     # Prevent the object from receiving messages from ANY thread; used for
     # objects "in transit" between threads, e.g. on a channel or queue
     #
+    # @param [ThreadBoundObject] tbo the TBO to unbind
+    # @return [ThreadBoundObject] the same object
+    # @raise BindingError if bound by another thread
     def self.nullbind(tbo)
       tbo.__nullbind
     end
 
+    # Query whether the ThreadBoundObject is bound to a thread
+    #
+    # @param [ThreadBoundObject] tbo the TBO to unbind
+    # @return [Boolean] true if the TBO is bound
     def self.bound?(tbo)
       tbo.__bound?
     end
@@ -52,62 +67,91 @@ module ZeevexConcurrency
     # Prevent the object from receiving messages from ANY thread; used for
     # objects "in transit" between threads, e.g. on a channel or queue
     #
-    #
+    # @param [Object] obj the object to make bindable via wrapping
+    # @param [Thread, :unbound] thread the Thread to bind to, nil for current thread, or
+    #    the symbol :unbound to leave the TBO unbound (default)
     def initialize(obj, thread = :unbound)
       super(obj)
       __bind_to_thread(thread) if thread != :unbound
     end
 
+    # @api private
     def __getobj__(ignore_binding = false)
       __check_binding unless ignore_binding
       super()
     end
 
+    # @api private
     def __setobj__(obj)
       raise "__setobj__ not supported"
     end
 
     #
-    # Given no or a nil argument, binds to the current thread
+    # Eagerly bind this object to a thread. If nil or no thread arg provided,
+    # binds to current thread.
     #
+    # @param [Thread] thread the Thread to bind to, or nil for Thread.current
+    # @return [ThreadBoundObject] self
+    #
+    # @api private
     def __bind_to_thread(thr = nil)
       thr ||= Thread.current
       raise ArgumentError, "Must provide thread" unless thr.is_a?(Thread)
       raise BindingError,  "Object is already bound" if __bound?
       __check_bindability
       @bound_thread_id = thr.__id__
+      self
     end
 
+    #
+    # Free the object from a binding; will auto-bind to first thread to send
+    # it a message
+    #
+    # @return [ThreadBoundObject] self
+    #
+    # @api private
+    # @see ThreadBoundObject.unbind
+    # @raise BindingError if bound by another thread
     def __unbind
       __check_bindability
       @bound_thread_id = nil
+      self
     end
 
     #
     # Bind this to a non-existent value so that it cannot be invoked from anywhere;
     # unbind and bind can override this
     #
+    # @api private
+    # @return [ThreadBoundObject] self
+    # @raise BindingError if bound by another thread
     def __nullbind
       __check_bindability
       # this can never match Thread.current
       @bound_thread_id = :none
+      self
     end
 
+    # @api private
+    # @see ThreadBoundObject.bound?
     def __bound?
       @bound_thread_id && @bound_thread_id != :none
     end
 
+    # @api private
+    # @see ThreadBoundObject.bindable?
     def __bindable?
       @bound_thread_id.nil? || @bound_thread_id == :none
     end
 
     private
 
-
     #
     # Successful if thread unbound or this method is being called from bound thread
     #    if unbound, auto-bind this object to the calling thread
-    # raise BindingError otherwise
+    #
+    # @api private
+    # @raise BindingError 
     #
     def __check_binding
       case @bound_thread_id
@@ -120,6 +164,9 @@ module ZeevexConcurrency
       end
     end
 
+    #
+    # @api private
+    # @raise BindingError
     def __check_bindability
       return true if @bound_thread_id == :none || @bound_thread_id.nil?
       return true if @bound_thread_id == Thread.current.__id__
