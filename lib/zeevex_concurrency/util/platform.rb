7998f64d2b183a51cc2ba834673e3ddd1d87f904
Robert Sanders <robert@zeevex.com>
2013-01-23 15:36:42 -0500

    new Util::Platform class for feature detection and platform support

---
+++ lib/zeevex_concurrency/util/platform.rb
@@ -0,0 +1,134 @@
+require 'zeevex_concurrency'
+require 'set'
+
+module ZeevexConcurrency
+  module Util
+    module Platform
+      @@features = Set.new
+
+      def self.has_gil?
+        feature?(:gil)
+      end
+
+      def self.has_thread_parallelism?
+        feature?(:thread_parallelism)
+      end
+
+      def self.feature?(feature)
+        features.include? feature
+      end
+
+      def self.initialize_features
+        return if @@features
+        (engine, engine_version, compat_version) = self.engine
+        @@features =
+          case engine
+          when :jruby
+            [:threads, :thread_parallelism, :native_threads, :ruby_18, :ruby_19,
+             :ffi, :java, :jit, :aot_compilation, :mvm]
+          when :mri_18
+            [:threads, :gil, :mri_18, :green_threads, :ruby_18]
+          when :yarv
+            [:threads, :fibers, :gil, :native_threads, :ruby_19]
+          when :rbx1
+            [:threads, :rbx, :rubinius, :rbx2, :gil, :green_threads, :ruby_18, :mvm]
+          when :rbx2
+            # Rubinius has both 1.8 and 1.9 modes (-X18 or -X19)
+            mode_features = compat_version.match(/^1\.8/) ? [:ruby_18] : [:ruby_19, :fibers]
+            [:threads, :rbx, :rubinius, :thread_parallelism, :native_threads, :jit, :mvm] + mode_features
+          when :macruby
+            [:threads, :thread_parallelism, :native_threads, :ruby_19,
+             :cocoa, :objc, :corefoundation, :osx,
+             :aot_compilation, :jit, :dtrace, :grand_central_dispatch, :ffi, :sandbox]
+          when :rubymotion
+            [:threads, :thread_parallelism, :native_threads, :ruby_19,
+             :cocoa, :objc, :corefoundation, :ios,
+             :aot_compilation, :dtrace, :grand_central_dispatch, :ffi, :sandbox,
+             :refcounting]
+          else
+            @@features = []
+            raise NotImplementedError, "Unsupported Ruby Platform: #{RUBY_ENGINE} - #{RUBY_DESCRIPTION}"
+          end
+      end
+
+      def self.features
+        initialize_features unless @@features
+        @@features.dup.freeze
+      end
+
+      def self.detect_features
+        res = []
+        res << :threads if defined?(Thread)
+        res << :fibers  if defined?(Fiber)
+        res += case RUBY_VERSION
+               when /^1\.8/ then [:ruby_18]
+               when /^1\.9/ then [:ruby_19]
+               when /^2\.0/ then [:ruby_19, :ruby_20]
+               end
+        res << :ree if RUBY_DESCRIPTION.include?("Ruby Enterprise Edition")
+        res << :cow_friendly_gc if GC.respond_to?(:copy_on_write_friendly=)
+        # TODO - test this on Windows
+        res << :fork if Kernel.respond_to?(:fork) && !defined?(JRuby)
+        res
+      end
+
+      def self.assert_supported_platforms!(platforms, *rest)
+        platforms = (Array(platforms) + rest).map &:to_sym
+        return true if platforms.include?(engine[0])
+        if block_given?
+          yield engine[0]
+        else
+          raise NotImplementedError, "Current platform not in supported list of #{platforms.inspect}"
+        end
+      end
+
+      #
+      # Return information about the current Ruby engine/runtime in a
+      # a 3-tuple of the form:
+      #    (ruby_engine, engine_version, ruby_lang_compat_version)
+      #
+      # Known engines (expressed as symbols):
+      #
+      # :mri_18  :: MRI/CRuby version 1.8.x
+      # :yarv    :: MRI/CRuby Ruby 1.9 or later
+      # :jruby   :: JRuby; all versions
+      # :rbx1    :: Rubinius version 1.x
+      # :rbx2    :: Rubinius version 2.x or later
+      # :macruby :: MacRuby, any version (tested on 0.12)
+      #
+      # Unhandled engines: mruby, maglev, rubymotion, ironruby, etc.
+      #
+      # @return [Array<Symbol, String, String>] a 3-tuple of the form
+      #    (ruby_engine, engine_version, ruby_lang_compat_version)
+      #
+      # @raise NotImplementedError if the engine is not currently supported
+      #
+      def self.engine
+        @@engine ||=
+          case RUBY_ENGINE
+          when "jruby"
+            [:jruby, JRUBY_VERSION, RUBY_VERSION]
+          when "ruby"
+            [RUBY_VERSION =~ /^1\.8/ ? :mri_18 : :yarv, RUBY_VERSION, RUBY_VERSION]
+          when "rbx"
+            version = Rubinius.version.match(/rubinius (\S+) \(/)[1]
+            [version =~ /^1\./ ? :rbx1 : :rbx2, version, RUBY_VERSION]
+          when "macruby"
+            [:macruby, MACRUBY_VERSION, RUBY_VERSION]
+          when "mruby", "maglev", "rubymotion", "ironruby", "kiji", "opal", "goruby"
+            raise NotImplementedError, "Haven't finished support for #{RUBY_ENGINE} yet"
+          else
+            raise NotImplementedError, "Unknown Ruby Platform: #{RUBY_ENGINE} - #{RUBY_DESCRIPTION}"
+          end
+      end
+
+      private
+
+      def self.reset_all
+        @@engine   = nil
+        @@features = nil
+      end
+
+    end
+  end
+end
---
+++ spec/util/platform_spec.rb
@@ -0,0 +1,172 @@
+require File.join(File.dirname(__FILE__), '../spec_helper')
+require 'zeevex_concurrency/util/platform.rb'
+require 'thread'
+
+describe ZeevexConcurrency::Util::Platform do
+  clazz = ZeevexConcurrency::Util::Platform
+  def pretend_yarv
+    stub_const("RUBY_ENGINE", "ruby")
+    stub_const('RUBY_VERSION', '1.9.2')
+  end
+
+  def pretend_mri_18
+    stub_const('RUBY_ENGINE', 'ruby')
+    stub_const('RUBY_VERSION', '1.8.7')
+  end
+
+  def pretend_jruby
+    stub_const('RUBY_ENGINE', 'jruby')
+    stub_const('RUBY_VERSION', '1.9.3')
+    stub_const('JRUBY_VERSION', '1.7.0')
+  end
+
+  def pretend_rbx1
+    stub_const('RUBY_ENGINE', 'rbx')
+    stub_const('RUBY_VERSION', '1.8.7')
+    rbobj = mock()
+    stub_const('Rubinius', rbobj)
+    rbobj.stub(:version).and_return('rubinius 1.2.4 (1.8.7 release 2011-07-05 JI) [x86_64-apple-darwin11.0.0]')
+  end
+
+  def pretend_rbx2
+    stub_const('RUBY_ENGINE', 'rbx')
+    stub_const('RUBY_VERSION', '1.9.3')
+    rbobj = mock()
+    stub_const('Rubinius', rbobj)
+    rbobj.stub(:version).and_return('rubinius 2.0.0rc1 (1.9.3 release 2012-11-02 JI) [x86_64-apple-darwin12.2.1]')
+  end
+
+  def pretend_macruby
+    stub_const('RUBY_ENGINE', 'macruby')
+    stub_const('RUBY_VERSION', '1.9.2')
+    stub_const('MACRUBY_VERSION', '0.12')
+  end
+
+  def pretend_mruby
+    stub_const('RUBY_ENGINE', 'mruby')
+    stub_const('RUBY_VERSION', '1.9.2')
+  end
+
+  def pretend_rubymotion
+    stub_const('RUBY_ENGINE', 'rubymotion')
+    stub_const('RUBY_VERSION', '1.9.3')
+  end
+
+  def pretend_to_be(platform)
+    send "pretend_#{platform}".to_sym
+  end
+
+  supported_platforms = [:mri_18, :yarv, :jruby, :rbx1, :rbx2, :macruby]
+  unsupported_platforms = [:mruby, :rubymotion]
+
+  before :each do
+    ZeevexConcurrency::Util::Platform.send :reset_all
+  end
+
+  context '.engine' do
+    subject do
+      ZeevexConcurrency::Util::Platform.engine
+    end
+    context 'on MRI 1.9 / YARV' do
+      before do
+        pretend_yarv
+      end
+      it 'should return the current Ruby engine' do
+        subject[0].should == :yarv
+      end
+      it 'should return the current Ruby engine version' do
+        subject[1].should == '1.9.2'
+      end
+      it 'should return the current Ruby compatibility version' do
+        subject[2].should == '1.9.2'
+      end
+    end
+    context 'on MRI 1.8' do
+      before do
+        pretend_mri_18
+      end
+      it 'should return the current Ruby engine' do
+        subject[0].should == :mri_18
+      end
+      it 'should return the current Ruby engine version' do
+        subject[1].should == '1.8.7'
+      end
+      it 'should return the current Ruby compatibility version' do
+        subject[2].should == '1.8.7'
+      end
+    end
+    context 'on JRuby' do
+      before do
+        pretend_jruby
+      end
+      it 'should return the current Ruby engine' do
+        subject[0].should == :jruby
+      end
+      it 'should return the current Ruby engine version' do
+        subject[1].should == '1.7.0'
+      end
+      it 'should return the current Ruby compatibility version' do
+        subject[2].should == '1.9.3'
+      end
+    end
+  end
+
+  context '.features' do
+    supported_platforms.each do |platform|
+      it 'should return a list of feature names' do
+        pretend_to_be platform
+        clazz.features.should be_a(Array)
+        clazz.features.should_not be_empty
+      end
+      it 'should return a list of symbols' do
+        pretend_to_be platform
+        clazz.features.reject {|x| x.is_a?(Symbol) }.should be_empty
+      end
+    end
+
+    it 'should cache the list at first invocation' do
+      clazz.should_receive(:initialize_features).once.and_call_original
+      clazz.features
+      clazz.features
+    end
+    it 'should return a frozen copy of the list' do
+      clazz.features.should be_frozen
+    end
+  end
+
+  context '.feature?' do
+    before do
+      pretend_to_be :mri_18
+    end
+
+    it 'should return true if a supported feature is given' do
+      clazz.feature?(:threads).should be_true
+    end
+    it 'should return false if an unsupported feature is given' do
+      clazz.feature?(:fibers).should be_false
+    end
+  end
+
+  context '.assert_supported_platforms!' do
+    before do
+      pretend_to_be :jruby
+    end
+
+    it 'should return true if current platform is supported' do
+      clazz.assert_supported_platforms!(:jruby, :yarv).should == true
+    end
+    it 'should raise NotImplementedError if current platform is not supported' do
+      expect { clazz.assert_supported_platforms!(:mri_18, :yarv) }.
+        to raise_error(NotImplementedError)
+    end
+    it 'should execute the provided failure block if current platform is not supported' do
+      obj = mock()
+      obj.should_receive(:on_platform).with(:jruby)
+      clazz.assert_supported_platforms!(:mri_18, :yarv) do |platform|
+        obj.on_platform :jruby
+      end
+    end
+
+  end
+end
+
