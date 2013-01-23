62de102cc1d892d8c3713ae70e850e00b117aa28
Robert Sanders <robert@zeevex.com>
2013-01-23 13:18:27 -0500

    set timeouts around specs.
    by default it's 60 seconds, but a group or example can have :timeout => X
    metadata to set it to X seconds

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,18 +14,38 @@ RSpec.configure do |config|
     puts "Running spec suite on #{RUBY_VERSION}"
   end
 
-  config.around :each do |example|
+  config.around :each do |exproc|
     completed = false
-    name = example.metadata.full_description
+    exmetadata = exproc.metadata
+    name = exmetadata.full_description
+    t_start = Time.now
     thr = Thread.new do
-      sleep 20
-      unless completed
-        puts "*** Example #{name} is taking too long to run! ***"
+      begin
+        result = exproc.run
+        [result, nil]
+      rescue
+        completed = true
+        [nil, $!]
       end
     end
-    example.run
-    completed = true
-    thr.kill
+
+    timeout = exmetadata[:test_timeout] || $global_test_timeout || 60
+
+    if thr.join(timeout)
+      (result, exception) = thr.value
+      raise exception if exception
+      result
+    else
+      runtime = Time.now - t_start
+      puts "\n*** Example #{name} at #{example.location} took too long to run (timeout=#{timeout}), DEADLOCK?, aborting test! ***\n"
+      Thread.new { timeout(10) { thr.kill } }
+      message = "example declared deadlocked after #{timeout} seconds"
+      begin
+        raise Timeout::Error, message
+      rescue
+        @example.set_exception($!, message)
+      end
+    end
   end
 
   config.before :each do |context|
